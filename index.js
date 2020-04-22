e9209471ac1812cbbbfae4b85e5ff9c663bf1142
Myrdrahl <even.juberg@artsdatabanken.no>
2020-04-22 15:41:02 +0200

    Tror vi går for fetch.

--- index.js
+++ index.js
@@ -1,37 +1,22 @@
 const fs = require('fs')
 const kartlag = JSON.parse(fs.readFileSync('kartlag.json'))
-let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-let wmsreq = new XMLHttpRequest()
-
-    /* wmsreq.open('GET', "https://gis3.NVE.no/map/services/VerneplanforVassdrag/MapServer/WmsServer?request=GetCapabilities&service=WMS", true)
-wmsreq.send()
-wmsreq.addEventListener("readystatechange", processRequest, false)
-wmsreq.onreadystatechange = processRequest
-function processRequest(e) {
-    if (wmsreq.readyState == 4 && wmsreq.status == 200) {
-        console.log("Yay")
-    } 
-    else {
-        console.log("Fail")
-    }
-} */
-console.log(kartlag)
-console.log(Object.keys(kartlag))
+const fetch = require("node-fetch")
 
 Object.keys(kartlag).forEach(key => 
-    {wmsreq.open('GET', kartlag[key].wmsurl, false)
-    wmsreq.send()
-    wmsreq.addEventListener("readystatechange", processRequest, false)
-    wmsreq.onreadystatechange = processRequest
-    function processRequest(e) {
-    if (wmsreq.readyState == 4 && wmsreq.status == 200) {
-        console.log(key,"Yay", wmsreq.readyState, wmsreq.status)
-        console.log(kartlag[key].wmsurl)
-    } 
-    else if (wmsreq.readyState == 4 && wmsreq.status !== 200 && wmsreq.responseText == '') {
-        console.log("Do nothing")
+    {const request = async () => {
+        const response = await fetch(kartlag[key].wmsurl)
+        if (response.status == 200) {
+            console.log("Requested OK")
+        } else {
+            console.log("Requested not OK!")
+        }
     }
-}}
+    request()
+    }
     
     )
 
+    
+    
+    
+    
