54a43cbf1ec50b71d141a1f39c4539c730e7fa52
Myrdrahl <even.juberg@artsdatabanken.no>
2020-04-24 10:34:32 +0200

    Finally, posting works.

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const fs = require('fs')
 const kartlag = JSON.parse(fs.readFileSync('kartlag.json'))
 const fetch = require("node-fetch")
+const config = require("./config/config.json")
+
 
 Object.keys(kartlag).forEach(key => 
     {const request = async () => {
         const response = await fetch(kartlag[key].wmsurl)
+        const data = {
+            "text": "Hello"
+        }
         if (response.status == 200) {
             console.log("Requested OK")
         } else {
             console.log("Requested not OK!",kartlag[key].tittel ,kartlag[key].wmsurl)
+            fetch(config.addressAndToken, {
+                method: 'POST',
+                body: JSON.stringify(data)
+            })
         }
     }
     request()
