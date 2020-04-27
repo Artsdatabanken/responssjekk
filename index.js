53ed78dbc94af46304064a375460b99f5f15e29a
Even <even.juberg@ntnu.no>
2020-04-27 09:58:29 +0200

    Fixed indents and quotes.

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 const fs = require('fs')
 const kartlag = JSON.parse(fs.readFileSync('kartlag.json'))
-const fetch = require("node-fetch")
-const config = require("./config/config.json")
+const fetch = require('node-fetch')
+const config = require('./config/config.json')
 
-
-Object.keys(kartlag).forEach(key => 
-    {const request = async () => {
-        const response = await fetch(kartlag[key].wmsurl)
-        const data = {
-            "text": "Jeg virker ikke: " + kartlag[key].tittel + ' ' + kartlag[key].wmsurl
-        }
-        if (response.status == 200) {
-            console.log("Requested OK")
-        } else {
-            console.log("Requested not OK!",kartlag[key].tittel ,kartlag[key].wmsurl)
-            fetch(config.addressAndToken, {
-                method: 'POST',
-                body: JSON.stringify(data)
-            })
-        }
+Object.keys(kartlag).forEach(key => {
+  const request = async () => {
+    const response = await fetch(kartlag[key].wmsurl)
+    const data = {
+      'text': 'Jeg virker ikke: ' + kartlag[key].tittel + ' ' + kartlag[key].wmsurl
     }
-    request()
+    if (response.status === 200) {
+      console.log('Requested OK')
+    } else {
+      console.log('Requested not OK!', kartlag[key].tittel, kartlag[key].wmsurl)
+      fetch(config.addressAndToken, {
+        method: 'POST',
+        body: JSON.stringify(data)
+      })
     }
-    
-    )
-
-    
-    
-    
-    
+  }
+  request()
+}
+)
