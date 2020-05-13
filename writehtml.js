50675e2e703255a16282c6cb5546dea435ac13b8
Myrdrahl <even.juberg@artsdatabanken.no>
2020-05-13 14:42:29 +0200

    More styling.

--- writehtml.js
+++ writehtml.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
   let local_data = data
   //console.log(local_data)      
   Object.keys(local_data).forEach(hovedlag => {
-    document.getElementById('wrapper').innerHTML += '<div class="hovedlag" id="hovedlag"> ' + '<div style="display: inline"> ' + hovedlag + '</div>' + local_data[hovedlag].tittel + '<div style="display: inline"> ' + local_data[hovedlag].wmsurl + '</div> ' + local_data[hovedlag].status + '<div style="display: inline"> ' + local_data[hovedlag].timeStamp + '</div>' + '</div>'
+    document.getElementById('wrapper').innerHTML += '<div class="hovedlag" id="hovedlag"> ' + '<div id="hovedlag"> ' + hovedlag + '</div>' + '<div id=hovedlag>' + local_data[hovedlag].tittel + '</div>'  + '<div id="hovedlag"> ' + local_data[hovedlag].wmsurl + '</div> ' + '<div id="hovedlag">' + local_data[hovedlag].status + '</div>' + '<div id="hovedlag">' + local_data[hovedlag].timeStamp + '</div>' + '</div>'
     if (local_data[hovedlag].underlag) {
       Object.keys(local_data[hovedlag].underlag).forEach(underlagKey => {
-      document.getElementById('hovedlag').innerHTML += '<div class="underlag" id="underlag"> ' + '<div style="display: inline"> ' + hovedlag + '-' + underlagKey + '</div>' + '<div style="display: inline"> ' + local_data[hovedlag].underlag[underlagKey]["tittel"] + '</div>' + '<div style="display: inline"> ' + local_data[hovedlag].underlag[underlagKey]["legendeurl"] + '</div>' + '</div>'
+      document.getElementById('hovedlag').innerHTML += '<div class="underlag"> ' + '<div id="underlag"> ' + hovedlag + '-' + underlagKey + '</div>' + '<div id="underlag">' + local_data[hovedlag].underlag[underlagKey]["tittel"] + '</div>' + '<div id="underlag">' + local_data[hovedlag].underlag[underlagKey]["legendeurl"] + '</div>' + '</div>'
       })
        
     }
   }
   )
 }
-)
+) 
