8b1196390370ba8375c84ad0e62c661f4994d37e
Myrdrahl <even.juberg@artsdatabanken.no>
2020-05-14 13:32:18 +0200

    Trikser litt med css.

--- writehtml.js
+++ writehtml.js
@@ -4,31 +4,32 @@ console.log("running")
 document.addEventListener("DOMContentLoaded", function(event) {
 
   console.log("JEG KJØRER")
-  let local_data = data;
+  let local_data = data
   Object.keys(local_data).forEach(hovedlag => {
     let lag = local_data[hovedlag];
-    let string = '<div class="hovedlag"> ';
-    string += '<span> ' + hovedlag + '</span>';
-    string += '<span>' + lag.tittel + '</span>';
-    string += '<span>' + lag.wmsurl + '</span> ';
-    string += '<span>' + lag.status + '</span>';
+    let string = '<div class="hovedlag"> '
+    string += '<span>'  + hovedlag + '</span>'
+    string += '<span>' + lag.tittel + '</span>'
+    string += '<span>' + lag.wmsurl + '</span> '
+    string += '<span>' + lag.status + '</span>'
     string += '<span>' + lag.timeStamp + '</span>'
-    string += '</div>';
+    string += '</div>'
 
-    document.getElementById('wrapper').innerHTML +=  string;
+    document.getElementById('wrapper').innerHTML +=  string
 
 
     if (lag.underlag) {
       Object.keys(lag.underlag).forEach(underlagKey => {
 
-      let string = "";
-      string += '<div class="underlag"> ';
-      string += '<span> ' + hovedlag + '-' + underlagKey + '</span>';
-      string += '<span>' + local_data[hovedlag].underlag[underlagKey]["tittel"] + '</span>' ;
-      string += '<span>' + local_data[hovedlag].underlag[underlagKey]["legendeurl"] + '</span>';
-      string += '</div>';
+      let string = ""
+      let ulag = local_data[hovedlag].underlag[underlagKey]
+      string += '<div class="underlag"> '
+      string += '<span> ' + hovedlag + '-' + underlagKey + '</span>'
+      string += '<span>' + ulag["tittel"] + '</span>' 
+      string += '<span>' + ulag["legendeurl"] + '</span>'
+      string += '</div>'
 
-      document.getElementById('wrapper').innerHTML +=  string;
+      document.getElementById('wrapper').innerHTML +=  string
       })
 
     }
